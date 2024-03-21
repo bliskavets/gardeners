e57d3cf46b667d9a5acf535f2d1c4f31169e9ffd
YuryHrytsuk <yuryhrytsuk@gmail.com>
2024-03-21 21:31:58 +0100

    fixes

--- streamlit/app/insights.py
+++ streamlit/app/insights.py
@@ -3,6 +3,7 @@ from maps import show_burned_map
 from maps import show_lc_ts_map
 from maps import show_side_by_side_tree_lc_map
 
+
 def main():
     st.markdown("# 360 interactive insights")
 
@@ -12,11 +13,7 @@ def main():
     st.image("/content/burn_rate_history.gif")
     st.markdown("<hr>", unsafe_allow_html=True)
 
-    st.write("##### Normalized Difference Vegetation Index changes")
-    st.image("/content/ndvi.gif")
-    st.markdown("<hr>", unsafe_allow_html=True)
-
-    st.write("##### Land cover 2003 vs 2020")
+    st.write("### Land cover 2003 vs 2020")
     show_side_by_side_tree_lc_map()
     st.markdown("<hr>", unsafe_allow_html=True)
 
@@ -27,29 +24,35 @@ def main():
 
     with col1:
         st.image("/content/lc_type_1.gif")
-        st.markdown("<text style='text-align: center;'>Annual IGBP</text>", unsafe_allow_html=True)
+        st.markdown(
+            "<text style='text-align: center;'>Annual IGBP</text>", unsafe_allow_html=True)
 
     with col2:
         st.image("/content/lc_type_2.gif")
-        st.markdown("<text style='text-align: center;'>Annual UMD</text>", unsafe_allow_html=True)
+        st.markdown(
+            "<text style='text-align: center;'>Annual UMD</text>", unsafe_allow_html=True)
 
     with col3:
         st.image("/content/lc_type_3.gif")
-        st.markdown("<text style='text-align: center;'>Annual LAI</text>", unsafe_allow_html=True)
-
+        st.markdown(
+            "<text style='text-align: center;'>Annual LAI</text>", unsafe_allow_html=True)
 
     with col4:
         st.image("/content/lc_type_4.gif")
-        st.markdown("<text style='text-align: center;'>Annual BGC</text>", unsafe_allow_html=True)
-    
+        st.markdown(
+            "<text style='text-align: center;'>Annual BGC</text>", unsafe_allow_html=True)
+
     st.markdown("<hr>", unsafe_allow_html=True)
 
-    st.write("##### Burned map")
-    show_burned_map()
-    st.markdown("<hr>", unsafe_allow_html=True)
-    
-    st.write("##### Land cover 2014-2020")
+    st.write("### Land cover 2014-2020")
     show_lc_ts_map()
+    st.markdown("<hr>", unsafe_allow_html=True)
+
+    st.write("### Normalized Difference Vegetation Index")
+    st.image("/content/ndvi.gif")
+
+    st.write("### Burned map")
+    show_burned_map()
     st.markdown("<hr>", unsafe_allow_html=True)
 
 
