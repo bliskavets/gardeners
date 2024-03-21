603e79cd74e6287a77282862a8b6b18d85b7f7f2
Aleksandr Semeniuk <gtv.blame@gmail.com>
2024-03-21 23:26:21 +0100

    Removed API tocken

--- backend/app/chatgpt.py
+++ backend/app/chatgpt.py
@@ -1,7 +1,7 @@
 import openai
 
 client = openai.OpenAI(    
-    api_key="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",
+    api_key="secret",
 )
 
 def get_completion_text(prompt, temperature=0.7, model="gpt-3.5-turbo"):
