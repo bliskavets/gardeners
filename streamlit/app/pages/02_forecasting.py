6335131fc410e111de1176f2160a170a7b953377
YuryHrytsuk <yuryhrytsuk@gmail.com>
2024-03-21 22:53:35 +0100

    fixes

--- streamlit/app/pages/02_forecasting.py
+++ streamlit/app/pages/02_forecasting.py
@@ -7,17 +7,6 @@ import ee
 # ee.Initialize(project='gardeners-417908')
 
 def main():
-    custom_css = """
-        <style>
-            [data-testid="stSidebarNavLink"] {
-                font-size: 25px;
-            }
-        </style>
-    """
-
-    # Inject custom CSS with st.markdown
-    st.markdown(custom_css, unsafe_allow_html=True)
-    
     st.markdown("# Burned area projection 2050")
     st.sidebar.markdown("# Land degradation in Brazil")
     st.image("/content/burned_area.gif")
